c28455c0fbdf3e778387a9e4aaad4ed9f69e87be
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-05-12 22:24:34 +0800

    Add gender for user model and table

--- database/migrations/20250313141544_users.ts
+++ database/migrations/20250313141544_users.ts
@@ -1,5 +1,5 @@
 import type { Knex } from "knex";
-import {STATUSES} from "../../src/models/user.model";
+import {GENDERS, STATUSES} from "../../src/models/user.model";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', table => {
@@ -8,10 +8,13 @@ export async function up(knex: Knex): Promise<void> {
         table.string('first_name', 100).nullable();
         table.string('middle_name', 100).nullable();
         table.string('last_name', 100).nullable();
+        table.enum('gender', GENDERS).nullable();
 
         table.text('address', 'tinytext').nullable();
         table.string('phone', 22).unique().nullable();
+        table.boolean('is_phone_verified').defaultTo(0).notNullable();
         table.string('email', 180).unique().nullable();
+        table.boolean('is_email_verified').defaultTo(0).notNullable();
 
         // user type
         table.integer('role_id').unsigned().notNullable();
@@ -24,10 +27,10 @@ export async function up(knex: Knex): Promise<void> {
         table.text('password', 'tinytext').nullable();
         table.enum('status', STATUSES).defaultTo('Activated');
 
-        table.text('comments', 'tinytext').nullable();
-
         table.integer('login_tries', 2).defaultTo(0);
         table.dateTime('failed_login_expired_at').nullable();
+
+        table.text('comments', 'tinytext').nullable();
 
         table.dateTime('created_at').nullable();
         table.dateTime('updated_at').nullable();
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,7 @@ import {Knex} from "knex";
 
 const TABLE_NAME = 'users';
 export const STATUSES = ['Pending', 'Activated', 'Suspended', 'Deleted', 'Deactivated'];
+export const GENDERS = ['Male', 'Female']
 
 export function UserModel(knex: Knex) {
     return {
