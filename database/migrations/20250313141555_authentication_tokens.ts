cd8f80797133af06e39f3b02260b27504c60c22f
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-20 05:51:59 -0400

    Remove: Remove unused columns for custom token replace by JWT

--- database/migrations/20250313141555_authentication_tokens.ts
+++ database/migrations/20250313141555_authentication_tokens.ts
@@ -10,12 +10,6 @@ export async function up(knex: Knex): Promise<void> {
             .onUpdate('CASCADE')
             .onDelete('CASCADE');
 
-        table.string('token', 180).unique().notNullable();
-
-        // two-factor authentication requirement
-        table.boolean('is_tfa_required').defaultTo(0).notNullable();
-        table.boolean('is_tfa_verified').defaultTo(0).notNullable();
-
         table.dateTime('created_at').nullable();
         table.dateTime('updated_at').nullable();
         table.dateTime('expired_at').nullable();
