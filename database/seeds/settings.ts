78079e95857bce2bc03df66ace8762ef85fd4763
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-20 05:52:35 -0400

    Remove: Delete unused columns for custom token

--- database/seeds/settings.ts
+++ database/seeds/settings.ts
@@ -8,7 +8,6 @@ export async function seed(knex: Knex): Promise<void> {
     await knex("settings").insert([
         {name: 'Max Login Tries', slug: 'mx_log_try', value: '5', type: 'integer', is_public: 0},
         {name: 'Failed Login Tries Lockout Period', slug: 'lck_prd', value: '5', type: 'integer', is_public: 0},
-        {name: 'Authentication Token Key Length', slug: 'tkn_lth', value: '62', type: 'integer', is_public: 0},
         {name: 'Authentication Token Expiration', slug: 'tkn_exp', value: '15', type: 'integer', is_public: 0},
         {name: 'TFA Required', slug: 'tta_req', value: '0', type: 'boolean', is_public: 0},
         {name: 'TFA Email Sender', slug: 'tta_eml_snd', value: '', type: 'string', is_public: 0},
