d9b35d38910b7f905c7c4052b7814228c74b4111
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-22 00:31:56 -0400

    Update: Change types

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,15 +1,27 @@
 import {UserInterface} from "../../interfaces/user.interface";
 import {AuthenticationTokenInterface} from "../../interfaces/authentication-token.interface";
-import {JwtPayload} from "jsonwebtoken";
+import {JwtExtendedPayload} from "../../models/authentication-token.model";
 
 declare global {
     namespace Express {
         interface Request {
             credentials: {
-                jwt: JwtPayload;
+                jwt: JwtExtendedPayload;
                 user: () => Promise<UserInterface>;
                 authentication: () => Promise<AuthenticationTokenInterface>;
-            }; // Adding a custom property to Request
+            };
+
+            sanitize: {
+                body: {
+                    get: (key: string, defaults?: any) => any;
+                    only: (keys: string[]) => Record<string, any>;
+                    numeric: (key: string, defaults?: any) => any;
+                },
+                query: {
+                    get: (key: string, defaults?: any) => any;
+                    numeric: (key: string, defaults?: any) => any;
+                }
+            };
         }
     }
 }
