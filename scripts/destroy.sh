f7bae54c6e8709f2f494dcd5f05786ab06c1d406
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-23 00:52:39 -0400

    Add: Create a network name

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,4 +1,4 @@
 docker container stop express-boilerplate-backend
 docker container rm express-boilerplate-backend
 docker image rm express-boilerplate/backend
-docker network rm express-boilerplate_default
+docker network rm expressjs-boilerplate-network
