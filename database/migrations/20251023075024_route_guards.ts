eaa16b39320527142de9af3c5c7e1a6150907373
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-28 10:28:58 -0400

    Add: Create a new table for route guard

---
+++ database/migrations/20251023075024_route_guards.ts
@@ -0,0 +1,21 @@
+import type {Knex} from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("route_guards", table => {
+        table.increments("id").primary();
+
+        table.integer("role_id").unsigned().index().notNullable();
+        table.foreign("role_id")
+            .references("roles.id")
+            .onUpdate("CASCADE")
+            .onDelete("CASCADE");
+        table.string("route", 100).notNullable();
+
+        table.dateTime("created_at").index().nullable();
+        table.dateTime("updated_at").nullable();
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("route_guards");
+}
