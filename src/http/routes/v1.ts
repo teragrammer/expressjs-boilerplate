fda277005e069109291cd3be548e57ac1226e2d4
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-31 10:54:20 -0400

    Edit: Change route permissions

--- src/http/routes/v1.ts
+++ src/http/routes/v1.ts
@@ -10,6 +10,7 @@ import AccountController from "../controllers/account.controller";
 import SettingController from "../controllers/setting.controller";
 import RoleController from "../controllers/role.controller";
 import UserController from "../controllers/user.controller";
+import RouteGuardController from "../controllers/route-guard.controller";
 
 const router = Router();
 
@@ -27,24 +28,29 @@ export default () => {
     router.put("/account/information", [AuthenticationMiddleware(), TwoFactorAuthenticationMiddleware()], AccountController.information);
     router.put("/account/password", [AuthenticationMiddleware(), TwoFactorAuthenticationMiddleware()], AccountController.password);
 
-    router.get("/settings", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], SettingController.browse);
+    router.get("/settings", [AuthenticationMiddleware(), PermissionMiddleware("settings:browse")], SettingController.browse);
     router.get("/settings/values", [AuthenticationMiddleware()], SettingController.values);
-    router.get("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], SettingController.view);
-    router.post("/settings", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], SettingController.create);
-    router.put("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], SettingController.update);
-    router.delete("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], SettingController.delete);
+    router.get("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware("settings:view")], SettingController.view);
+    router.post("/settings", [AuthenticationMiddleware(), PermissionMiddleware("settings:create")], SettingController.create);
+    router.put("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware("settings:update")], SettingController.update);
+    router.delete("/settings/:id", [AuthenticationMiddleware(), PermissionMiddleware("settings:delete")], SettingController.delete);
 
-    router.get("/roles", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], RoleController.browse);
-    router.get("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], RoleController.view);
-    router.post("/roles", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], RoleController.create);
-    router.put("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], RoleController.update);
-    router.delete("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], RoleController.delete);
+    router.get("/roles", [AuthenticationMiddleware(), PermissionMiddleware("roles:browse")], RoleController.browse);
+    router.get("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware("roles:view")], RoleController.view);
+    router.post("/roles", [AuthenticationMiddleware(), PermissionMiddleware("roles:create")], RoleController.create);
+    router.put("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware("roles:update")], RoleController.update);
+    router.delete("/roles/:id", [AuthenticationMiddleware(), PermissionMiddleware("roles:delete")], RoleController.delete);
 
-    router.get("/users", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], UserController.browse);
-    router.get("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], UserController.view);
-    router.post("/users", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], UserController.create);
-    router.put("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], UserController.update);
-    router.delete("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware(["admin"])], UserController.delete);
+    router.get("/route/guards", [AuthenticationMiddleware(), PermissionMiddleware("route-guards:browse")], RouteGuardController.browse);
+    router.get("/route/guards/:id", [AuthenticationMiddleware(), PermissionMiddleware("route-guards:view")], RouteGuardController.view);
+    router.post("/route/guards", [AuthenticationMiddleware(), PermissionMiddleware("route-guards:create")], RouteGuardController.create);
+    router.delete("/route/guards/:id", [AuthenticationMiddleware(), PermissionMiddleware("route-guards:delete")], RouteGuardController.delete);
+
+    router.get("/users", [AuthenticationMiddleware(), PermissionMiddleware("users:browse")], UserController.browse);
+    router.get("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware("users:view")], UserController.view);
+    router.post("/users", [AuthenticationMiddleware(), PermissionMiddleware("users:create")], UserController.create);
+    router.put("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware("users:update")], UserController.update);
+    router.delete("/users/:id", [AuthenticationMiddleware(), PermissionMiddleware("users:delete")], UserController.delete);
 
     return router;
 }
