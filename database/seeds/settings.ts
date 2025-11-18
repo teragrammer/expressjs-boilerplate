2957b58ff54dcb19d8376a0254875a59be3e130d
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-11-18 10:01:39 -0500

    Add setting for password recovery setting for sending email

--- database/seeds/settings.ts
+++ database/seeds/settings.ts
@@ -7,7 +7,11 @@ export async function seed(knex: Knex): Promise<void> {
         {name: 'Failed Login Tries Lockout Period', slug: 'lck_prd', value: '5', type: 'integer', is_public: 0},
         {name: 'Authentication Token Expiration', slug: 'tkn_exp', value: '15', type: 'integer', is_public: 0},
         {name: 'TFA Required', slug: 'tta_req', value: '0', type: 'boolean', is_public: 0},
+
         {name: 'TFA Email Sender', slug: 'tta_eml_snd', value: '', type: 'string', is_public: 0},
         {name: 'TFA Email Subject', slug: 'tta_eml_sbj', value: 'Your OTP Code', type: 'string', is_public: 0},
+
+        {name: "Password Recovery Email Sender", slug: "psr_eml_snd", value: "", type: "string", is_public: 0},
+        {name: "Password Recovery Email Subject", slug: "psr_eml_sbj", value: "Reset Your Password - Action Required", type: "string", is_public: 0},
     ]);
 }
