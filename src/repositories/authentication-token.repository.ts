47887484908f4aa259adc6d78988a7e9ae8fd9ca
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-11-19 10:05:00 -0500

    Create repository for authentication token for deleting of expired tokens

---
+++ src/repositories/authentication-token.repository.ts
@@ -0,0 +1,33 @@
+import {AuthenticationTokenModel} from "../models/authentication-token.model";
+import {DateUtil} from "../utilities/date.util";
+
+class AuthenticationTokenRepository {
+    private static instance: AuthenticationTokenRepository;
+
+    constructor() {
+    }
+
+    static getInstance(): AuthenticationTokenRepository {
+        if (!AuthenticationTokenRepository.instance) AuthenticationTokenRepository.instance = new AuthenticationTokenRepository();
+        return AuthenticationTokenRepository.instance;
+    }
+
+    insert(data: any) {
+        return AuthenticationTokenModel().table().returning("id").insert(data);
+    }
+
+    deleteExpiredByUserId(userId: number) {
+        return AuthenticationTokenModel().table()
+            .where("user_id", userId)
+            .where("expired_at", "<", DateUtil().sql())
+            .delete();
+    }
+
+    deleteAllByUserId(userId: number) {
+        return AuthenticationTokenModel().table()
+            .where("user_id", userId)
+            .delete();
+    }
+}
+
+export default AuthenticationTokenRepository.getInstance();
