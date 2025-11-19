1ba2da4ed189e8ae257d309d27aa62606c692ba5
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-11-19 10:01:50 -0500

    Add is_bypass_authorization column

--- src/interfaces/role.interface.ts
+++ src/interfaces/role.interface.ts
@@ -4,6 +4,7 @@ export interface RoleInterface {
     slug: string;
     description: string | null;
     is_public: number;
+    is_bypass_authorization: number;
     created_at: string | null;
     updated_at: string | null;
 }
